17f05252d4f2a6f8db53e73a1699ade28acfb300
Oespin1226 <oespinalcastillo@horizon.csueastbay.edu>
2022-03-14 13:40:50 -0700

    added Logout

--- Reci-Pie/HomePageViewController.swift
+++ Reci-Pie/HomePageViewController.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import Parse
 
 class HomePageViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate =  windowScene.delegate as? SceneDelegate else { return }
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 
