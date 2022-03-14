b6ea3abe15c3f6d8e04cccf87fde6bafc0f8a6b9
vnguyen162 <89062545+vnguyen162@users.noreply.github.com>
2022-03-13 23:54:35 -0700

    added password secure text

--- Reci-Pie/RapidAPI.swift
+++ Reci-Pie/RapidAPI.swift
@@ -15,9 +15,7 @@ class RapidAPI{
         "x-rapidapi-key": "a3cf0ea13dmsh6713b67976a9ab0p12310fjsn1e563b3c0834"
     ]
 
-    let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/images/analyze?imageUrl=https%3A%2F%2Fspoonacular.com%2FrecipeImages%2F635350-240x150.jpg")! as URL,
-                                            cachePolicy: .useProtocolCachePolicy,
-                                        timeoutInterval: 10.0)
+    let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/images/analyze?imageUrl=https%3A%2F%2Fspoonacular.com%2FrecipeImages%2F635350-240x150.jpg")! as URL,cachePolicy: .useProtocolCachePolicy,timeoutInterval: 10.0)
     
 }
     
